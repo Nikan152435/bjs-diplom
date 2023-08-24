fcc3b39f75da41422a21d145f84f2b47d9ce7439
NikaN <NikaN7602@gmail.com>
2023-08-24 21:36:30 +0200

    public/loginPage.js

--- public/loginPage.js
+++ public/loginPage.js
@@ -6,14 +6,14 @@ const userFormObject = new UserForm(); // создает объект класс
 userFormObject.loginFormCallback = (data) => { // чтобы не потерять контекс (this) лучше (проще)
   // использовать стрелочную ф-ию
   ApiConnector.login(data, response => {
-  // console.log(response); // проверяет какой объект возвращает сервер. 
+    // console.log(response); // проверяет какой объект возвращает сервер. 
 
-    if(response.success ) {// проверяет успешность запроса. 
+    if (response.success) {// проверяет успешность запроса. 
       location.reload(); // перезагрузка страницы (с переходом в личный кабинет),
-      
+
     } else {
       userFormObject.setLoginErrorMessage(response.error);
-       //метод у объекта/экземпляра класса userForm
+      //метод у объекта/экземпляра класса userForm
     };
   });
 };
@@ -23,16 +23,16 @@ userFormObject.loginFormCallback = (data) => { // чтобы не потерят
 
 userFormObject.registerFormCallback = (data) => { // Функция, которая будет обрабатывать
 
-ApiConnector.login(data, response => { //Вот тут не поняла, что менять?
+  ApiConnector.login(data, response => { //Вот тут не поняла, что менять?
     // console.log(response); // проверяет какой объект возвращает сервер. 
 
-    if(response.success === true) {// проверяет успешность запроса. Само значение свойства success является логическим (тип boolean), поэтому можно сократить данное условие до простого response.success (удалить === true)
+    if (response.success === true) {// проверяет успешность запроса. Само значение свойства success является логическим (тип boolean), поэтому можно сократить данное условие до простого response.success (удалить === true)
       location.reload(); // перезагрузка страницы (с переходом в личный кабинет), 
       //потому что логин/пароль были верные. Тут перезагрузка нужна?
 
     } else {
-userFormObject.setregisterErrorMessage(response.error);  //Вывод сообщений при регистрации
-//метод у объекта/экземпляра класса registerForm, 
+      userFormObject.setregisterErrorMessage(response.error);  //Вывод сообщений при регистрации
+      
     };
   });
 };
